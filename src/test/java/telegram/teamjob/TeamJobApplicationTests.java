4f1040dad9f637faa560190e70ec7072b6fb4b79
R30Radium <R30Radium@gmail.ru>
2022-11-08 09:58:04 +0400

    111

---
+++ src/test/java/telegram/teamjob/TeamJobApplicationTests.java
@@ -0,0 +1,13 @@
+package telegram.teamjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
