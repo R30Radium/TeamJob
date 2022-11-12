be8963e83b3826f378dbc356b91c2392290b822e
R30Radium <R30Radium@gmail.ru>
2022-11-12 10:17:55 +0400

    branches

--- src/main/java/telegram/teamjob/TeamJobApplication.java
+++ src/main/java/telegram/teamjob/TeamJobApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeamJobApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(TeamJobApplication.class, args);
     }
